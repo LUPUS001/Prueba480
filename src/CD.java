e1af7fe37099e6da73ac856597ed74358e4b92c0
LUPUS23 <antoniodaw23@gmail.com>
2025-01-17 12:56:16 +0100

    First Update

---
+++ src/CD.java
@@ -0,0 +1,14 @@
+public class CD {
+    private String nombre;
+    private int capacidad;
+    private String contenido;
+    private String tipo;
+    private String escribeDato;
+    private int velocidadGiro;
+
+    public CD(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
